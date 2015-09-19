04a3770e3e9cdd062969d1c3f11896c8c17307de
SinisterRectus <SinisterRectus@users.noreply.github.com>
2015-09-19 15:42:26 -0400

    Optimizations and bug fixes.
    Fixed a bug where the map would not properly fade-in.

--- client/cExtractionMap.lua
+++ client/cExtractionMap.lua
@@ -8,9 +8,6 @@ function Map:__init()
 	self.text_size = Render.Height * self.text_scale
 	
 	self.players = {}
-	
-	self.network_timer = Timer()
-	self.network_delay = 1000 -- milliseconds
 
 	self.extraction_enabled = true
 	self.extraction_speed = 5000 -- meters per second
@@ -22,7 +19,7 @@ function Map:__init()
 	self.render = false -- Do not change
 	self.labels = 0 -- Do not change
 	
-	self.inputs = {
+	self.actions = { -- Action to block while map is open
 		[3] = true,
 		[4] = true,
 		[5] = true,
@@ -40,92 +37,88 @@ function Map:__init()
 	}
 	
 	self.map = Image.Create(AssetLocation.Game, 'pda_map_dif.dds')
-	self.map:SetSize(Vector2(Render.Height, Render.Height))
-	self.map:SetPosition(Vector2(0.5 * Render.Width - 0.5 * self.map:GetSize().x,0))
-	
 	self.marker = Image.Create(AssetLocation.Game, "hud_icon_objective_dif.dds")
-	self.marker:SetSize(Vector2(1, 1) * Render.Height * 0.04)
-	
-	self.waypoint = Image.Create(AssetLocation.Game, "hud_icon_waypoint_dif.dds")
-	self.waypoint:SetSize(Vector2(1, 1) * Render.Height * 0.04)
-	
+	self.waypoint = Image.Create(AssetLocation.Game, "hud_icon_waypoint_dif.dds")	
 	self.heli = Image.Create(AssetLocation.Game, "hud_icon_heli_orange_dif.dds")
-	self.heli:SetSize(Vector2(1, 1) * Render.Height * 0.04)
 	
-	Events:Subscribe("MouseUp", self, self.OnMouseUp)
+	self:ResolutionChange({size = Render.Size})
+	
+	Events:Subscribe("MouseUp", self, self.Mouseup)
 	Events:Subscribe("KeyUp", self, self.ToggleMap)
-	Events:Subscribe("PostRender", self, self.OnPostRender)
-	Events:Subscribe("ResolutionChange", self, self.OnResolutionChange)
+	Events:Subscribe("PostRender", self, self.PostRender)
+	Events:Subscribe("ResolutionChange", self, self.ResolutionChange)
 	Events:Subscribe("LocalPlayerInput", self, self.InputBlock)
 
-	Network:Subscribe("PlayerUpdate", self, self.OnPlayerUpdate)
+	Network:Subscribe("PlayerUpdate", self, self.PlayerUpdate)
 
 end
 
 function Map:ToggleMap(args)
 
+	if self.extraction_sequence then return end
+	
 	local k = args.key
 	
 	if not self.render and k == VirtualKey.F2 and Game:GetState() == GUIState.Game then
 		Mouse:SetVisible(true)
 		self.render = true
-		Events:Fire("ShowMap") -- Can be used to communicate with other modules
-	elseif self.render and (k == VirtualKey.Escape or k == VirtualKey.F1 or k == VirtualKey.F2) and not self.extraction_sequence then
+		Events:Fire("MapShown") -- Can be used to communicate with other modules
+		Network:Send("MapShown")
+	elseif self.render and (k == VirtualKey.Escape or k == VirtualKey.F1 or k == VirtualKey.F2) then
 		Mouse:SetVisible(false)
 		self.render = false
-		Events:Fire("HideMap") -- Can be used to communicate with other modules
+		Events:Fire("MapHidden") -- Can be used to communicate with other modules
+		Network:Send("MapHidden")
 	end
 
 end
 
-function Map:OnMouseUp(args)
+function Map:Mouseup(args)
 
-	if self.render and Game:GetState() == GUIState.Game and not self.extraction_sequence then
+	if not self.render or not Game:GetState() == GUIState.Game or self.extraction_sequence then return end
 		
-		if self.extraction_enabled and args.button == 1 then
+	if self.extraction_enabled and args.button == 1 then
+
+		if not LocalPlayer:InVehicle() then
 	
-			if not LocalPlayer:InVehicle() then
-		
-				local position = self:MapToWorld(Mouse:GetPosition())
-				
-				if position.x >= -16384 and position.x <= 16384 and position.z >= -16384 and position.z <= 16384 then
+			local position = self:MapToWorld(Mouse:GetPosition())
+			
+			if position.x >= -16384 and position.x <= 16384 and position.z >= -16384 and position.z <= 16384 then
 
-					self.previous_position = LocalPlayer:GetPosition()
-					self.next_position = position
-					self.extraction_delay = Vector3.Distance(self.previous_position, self.next_position) / self.extraction_speed
-					self.extraction_sequence = Events:Subscribe("PreTick", self, self.ExtractionSequence)
-					self.world_fadeout_timer = Timer()
-					Game:FireEvent("ply.makeinvulnerable")
-					Mouse:SetVisible(false)
-
-				end
-				
-			else
-
-				Chat:Print("You cannot be extracted while in a vehicle.", Color.Silver)
+				self.previous_position = LocalPlayer:GetPosition()
+				self.next_position = position
+				self.extraction_delay = Vector3.Distance(self.previous_position, self.next_position) / self.extraction_speed
+				self.extraction_sequence = Events:Subscribe("PreTick", self, self.ExtractionSequence)
+				self.world_fadeout_timer = Timer()
+				Game:FireEvent("ply.makeinvulnerable")
+				Mouse:SetVisible(false)
 
 			end
 			
-		elseif args.button == 3 then
-		
-			local waypoint, exist = Waypoint:GetPosition()
-			
-			if exist then
-				Waypoint:Remove()
-			else 
-				Waypoint:SetPosition(self:MapToWorld(Mouse:GetPosition()))	
-			end
-			
-		elseif args.button == 2 then
-		
-			if self.labels == 2 then
-				self.labels = 0
-			else
-				self.labels = self.labels + 1
-			end
-				
+		else
+
+			Chat:Print("You cannot be extracted while in a vehicle.", Color.Silver)
+
 		end
 		
+	elseif args.button == 3 then
+	
+		local waypoint, exist = Waypoint:GetPosition()
+		
+		if exist then
+			Waypoint:Remove()
+		else 
+			Waypoint:SetPosition(self:MapToWorld(Mouse:GetPosition()))	
+		end
+		
+	elseif args.button == 2 then
+	
+		if self.labels == 2 then
+			self.labels = 0
+		else
+			self.labels = self.labels + 1
+		end
+			
 	end
 
 end
@@ -137,12 +130,10 @@ function Map:ExtractionSequence()
 	if self.world_fadeout_timer then
 		
 		if self.world_fadeout_timer:GetMilliseconds() > self.world_fadeout_delay then
-		
 			Network:Send("InitialTeleport", {position = self.next_position})
 			self.world_fadeout_timer = nil
 			self.extraction_timer = Timer()
 			self.teleporting = true
-	
 		end
 		
 	end
@@ -150,10 +141,8 @@ function Map:ExtractionSequence()
 	if self.teleporting then 
 	
 		if LocalPlayer:GetPosition() ~= self.previous_position then
-		
 			self.teleporting = false
 			self.loading = true
-			
 		end
 		
 	end
@@ -161,9 +150,7 @@ function Map:ExtractionSequence()
 	if self.loading then 
 		
 		if LocalPlayer:GetLinearVelocity() ~= Vector3.Zero then
-		
 			self.loading = false
-	
 		end
 		
 	end
@@ -171,10 +158,8 @@ function Map:ExtractionSequence()
 	if self.extraction_timer then
 		
 		if self.extraction_timer:GetSeconds() > self.extraction_delay then
-		
 			self.extraction_timer = nil
 			self.map_fadeout_timer = Timer()
-			
 		end
 		
 	end
@@ -184,14 +169,12 @@ function Map:ExtractionSequence()
 		local dt = self.map_fadeout_timer:GetMilliseconds()
 		local delay = self.map_fadeout_delay
 	
-		self.map:SetAlpha(1 - dt / delay)
-		self.marker:SetAlpha(1 - dt / delay)
-		self.waypoint:SetAlpha(1 - dt / delay)
+		self.map:SetAlpha(math.clamp(1 - dt / delay, 0, 1))
+		self.marker:SetAlpha(math.clamp(1 - dt / delay, 0, 1))
+		self.waypoint:SetAlpha(math.clamp(1 - dt / delay, 0, 1))
 	
 		if dt > delay then
-		
 			self.map_fadeout_timer = nil
-		
 		end
 		
 	end
@@ -201,7 +184,7 @@ function Map:ExtractionSequence()
 	
 		self.world_fadein_timer = Timer()
 		local ray = Physics:Raycast(Vector3(self.next_position.x, 2100, self.next_position.z), Vector3.Down, 0, 2100)
-		Network:Send("CorrectedTeleport", {position = ray.position})	
+		Network:Send("CorrectedTeleport", {position = ray.position})
 		
 	end
 	
@@ -257,7 +240,7 @@ function Map:YawToHeading(yaw)
 	end
 end
 
-function Map:OnPostRender()
+function Map:PostRender()
 
 	if Game:GetState() ~= GUIState.Game then return end
 	
@@ -270,7 +253,7 @@ function Map:OnPostRender()
 			
 			if dt < delay then
 				
-				Render:FillArea(Vector2.Zero, Render.Size, self:ColorAlpha(Color.Black, 255 * (dt / delay)))
+				Render:FillArea(Vector2.Zero, Render.Size, self:ColorA(Color.Black, 255 * (dt / delay)))
 		
 			end
 			
@@ -278,7 +261,7 @@ function Map:OnPostRender()
 		
 		if self.teleporting or self.loading or self.extraction_timer or self.map_fadeout_timer then
 		
-			Render:FillArea(Vector2.Zero, Render.Size, self:ColorAlpha(Color.Black, 255))
+			Render:FillArea(Vector2.Zero, Render.Size, Color.Black)
 			
 		end
 
@@ -289,7 +272,7 @@ function Map:OnPostRender()
 			
 			if dt < delay then
 		
-				Render:FillArea(Vector2.Zero, Render.Size, self:ColorAlpha(Color.Black, 255 * (1 - dt / delay)))
+				Render:FillArea(Vector2.Zero, Render.Size, self:ColorA(Color.Black, 255 * (1 - dt / delay)))
 				
 			end
 
@@ -300,49 +283,47 @@ function Map:OnPostRender()
 	if self.render then
 
 		self.map:Draw()
-		
+
 		if not self.extraction_sequence then
 		
-			if self.network_timer:GetMilliseconds() > self.network_delay then
-		
-				Network:Send("RequestUpdate")
-				self.network_timer:Restart()
-			
-			end
-		
 			for _,player in ipairs(self.players) do
-
-				local position = self:WorldToMap(player.position)
-				local color = player.color
-				
-				local str = player.name
 			
-				Render:FillCircle(position, Render.Height * 0.005, self:ColorAlpha(player.color, 220))
-				Render:DrawCircle(position, Render.Height * 0.005, Color.Black)
-				
-				if self.labels == 2 then
-				
-					if player.vehicle_name then
+				if player.id ~= LocalPlayer:GetId() then
 
-						local speed = math.floor(player.velocity:Length() * 3.6)
-						local heading = math.floor(self:YawToHeading(math.deg(player.angle.yaw)))
-						local altitude = math.floor(player.position.y + 200)
-						str = string.format("%s\n%s\n%s km/h : %s m : %s°", player.name, player.vehicle_name, speed, altitude, heading)
-						
-					else
+					local position = self:WorldToMap(player.pos)
+					local str = player.name
+				
+					Render:FillCircle(position, Render.Height * 0.005, self:ColorA(player.col, 220))
+					Render:DrawCircle(position, Render.Height * 0.005, Color.Black)
 					
-						str = string.format("%s\nOn-Foot", player.name)
+					if self.labels == 2 then
+					
+						if player.veh then
+
+							str = string.format(
+								"%s\n%s\n%i km/h : %i m : %i°", 
+								player.name, 
+								player.veh, 
+								player.vel:Length() * 3.6, 
+								player.pos.y + 200, 
+								self:YawToHeading(math.deg(player.ang.yaw))
+							)
+							
+						else
+						
+							str = string.format("%s\nOn-Foot", player.name)
+							
+						end
 						
 					end
 					
-				end
-				
-				if self.labels ~= 0 then
-				
-					Render:FillArea(position + Render.Size * 0.001, Vector2(1.05 * Render:GetTextWidth(str, self.text_size), Render:GetTextHeight(str, self.text_size)), self:ColorAlpha(Color.Black, 128))
-					Render:DrawText(position + Render.Size * 0.003 + Vector2(2, 2), str, self:ColorAlpha(Color.Black, 64), self.text_size)
-					Render:DrawText(position + Render.Size * 0.003 + Vector2(1, 1), str, self:ColorAlpha(Color.Black, 128), self.text_size)
-					Render:DrawText(position + Render.Size * 0.003, str, player.color, self.text_size)
+					if self.labels ~= 0 then
+					
+						Render:FillArea(position + Render.Size * 0.003, Vector2(Render:GetTextWidth(str, self.text_size), Render:GetTextHeight(str, self.text_size)), self:ColorA(Color.Black, 128))
+						Render:DrawText(position + Render.Size * 0.003 + Vector2.One, str, self:ColorA(Color.Black, 128), self.text_size)
+						Render:DrawText(position + Render.Size * 0.003, str, player.col, self.text_size)
+						
+					end
 					
 				end
 					
@@ -367,51 +348,42 @@ function Map:OnPostRender()
 				self.waypoint:Draw()
 			end
 		end
-
-	-- else
-	
-		-- local str = "Press F2 to open a full map."
-		-- local str_size = Render:GetTextSize(str, self.text_size)
-		-- Render:DrawText(Vector2(0.12 * Render.Width, 0.03 * Render.Height), str, Color.White, self.text_size)
 		
 	end
 	
 end
 
-function Map:ColorAlpha(color, alpha)
+function Map:ColorA(color, alpha)
 
 	return Color(color.r, color.g, color.b, alpha)
 
 end
 
-function Map:OnPlayerUpdate(args)
+function Map:PlayerUpdate(args)
 
 	self.players = args
 	
 end
 
-function Map:OnResolutionChange(args)
+function Map:ResolutionChange(args)
 
 	self.text_size = args.size.y * self.text_scale
 	self.map:SetSize(Vector2(args.size.y, args.size.y))
-	self.waypoint:SetSize(Vector2(1, 1) * args.size.y * 0.04)
-	self.marker:SetSize(Vector2(1, 1) * args.size.y * 0.04)
-	self.map:SetPosition(Vector2(0.5 * args.size.x - 0.5 * self.map:GetSize().x,0))
+	self.waypoint:SetSize(Vector2.One * args.size.y * 0.04)
+	self.marker:SetSize(Vector2.One * args.size.y * 0.04)
+	self.heli:SetSize(Vector2.One * args.size.y * 0.04)
+	self.map:SetPosition(Vector2(0.5 * args.size.x - 0.5 * self.map:GetSize().x, 0))
 
 end
 
 function Map:InputBlock(args)
 
-	local i = args.input
-
 	if self.extraction_sequence then
 		return false
 	end
 	
-	if self.render then
-		if self.inputs[i] then
-			return false
-		end
+	if self.render and self.actions[args.input] then
+		return false
 	end
 
 end
